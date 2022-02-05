75175b18bc8b2a397e1fb8529d4ac728aec612f1
C-P-G <claire.pfalzner-gibbon@stud.sbg.ac.at>
2022-02-05 17:37:53 +0100

    Update accidentsmain.py
    added intersection

--- accidentsmain.py
+++ accidentsmain.py
@@ -11,17 +11,23 @@ env.workspace = ""
 
 # Buffer streets
 # Set local variables
-inFeatures = "streets"
+in_features = "streets"
 # set output path
-outFeatureClass = "c:/output/output.gdb/multibufferstreets"
+out_feature_class = "c:/output/output.gdb/bufferstreets"
 buffer_distance_or_field = [200, meters]
-
+# actual buffer function
 arcpy.analysis.Buffer(in_features, out_feature_class, buffer_distance_or_field)
 
 
 # Intersect streets with accidents
-# Kernelfunction
-# print pdf
+in_features = ["bufferstreets", "wildlife_accidents"]
+out_feature_class = "output.gdb/intersection"
+join_attributes = "NO_FID"
+arcpy.analysis.Intersect(in_features, out_feature_class, {join_attributes}, {cluster_tolerance}, {output_type})
+
+
+# KERNELFUNCTION
+# PRINT PDF
 # output path (folder). Please change accordingly
 outPath = "C:\\Users\\clair\\Desktop\\WS_20\\GeoApp"
 #generation of an empty PDF master document
