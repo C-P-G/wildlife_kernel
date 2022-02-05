5d0394f7e75fe7ea16598498eef6f6085b63f989
C-P-G <claire.pfalzner-gibbon@stud.sbg.ac.at>
2022-02-05 08:26:08 +0100

    Create accidentsmain.py

---
+++ accidentsmain.py
@@ -0,0 +1,11 @@
+# Application Development Final project
+# Felix Reisinger & Claire Pfalzner-Gibbon
+# purpose: Kernelfunction wildlife accidents
+# created: 05.02.22
+# last update: 05.02.2022
+
+
+# Buffer datasets
+# Intersect streets with accidents
+# Kernelfunction
+# print pdf
