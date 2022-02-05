65a16072ec4f139443eecdcf938cbfb1cec6a4c3
C-P-G <claire.pfalzner-gibbon@stud.sbg.ac.at>
2022-02-05 17:30:22 +0100

    Update accidentsmain.py
    started on buffer function

--- accidentsmain.py
+++ accidentsmain.py
@@ -4,8 +4,25 @@
 # created: 05.02.22
 # last update: 05.02.2022
 
+import arcpy
+# Set environment settings
+env.workspace = ""
 
-# Buffer datasets
+
+# Buffer streets
+# Set local variables
+inFeatures = "streets"
+# set output path
+outFeatureClass = "c:/output/output.gdb/multibufferstreets"
+buffer_distance_or_field = [200, meters]
+
+arcpy.analysis.Buffer(in_features, out_feature_class, buffer_distance_or_field)
+
+
 # Intersect streets with accidents
 # Kernelfunction
 # print pdf
+# output path (folder). Please change accordingly
+outPath = "C:\\Users\\clair\\Desktop\\WS_20\\GeoApp"
+#generation of an empty PDF master document
+pdfDoc = arcpy.mp.PDFDocumentCreate(outPath + "")
