91cd665d4a2f626f19d22c56f881722562eb2d30
C-P-G <claire.pfalzner-gibbon@stud.sbg.ac.at>
2022-02-05 17:42:35 +0100

    Update accidentsmain.py

--- accidentsmain.py
+++ accidentsmain.py
@@ -13,10 +13,10 @@ env.workspace = ""
 # Set local variables
 in_features = "streets"
 # set output path
-out_feature_class = "c:/output/output.gdb/bufferstreets"
-buffer_distance_or_field = [200, meters]
+output_buffer = "c:/output/output.gdb/bufferstreets"
+buffer_distance = "200 meters"
 # actual buffer function
-arcpy.analysis.Buffer(in_features, out_feature_class, buffer_distance_or_field)
+arcpy.analysis.Buffer(in_features, output_buffer, buffer_distance)
 
 
 # Intersect streets with accidents
@@ -27,8 +27,12 @@ arcpy.analysis.Intersect(in_features, out_feature_class, {join_attributes}, {clu
 
 
 # KERNELFUNCTION
+# which one to use ?
+# https://pro.arcgis.com/de/pro-app/latest/tool-reference/geostatistical-analyst/an-overview-of-the-interpolation-toolset.htm
+
+
 # PRINT PDF
 # output path (folder). Please change accordingly
 outPath = "C:\\Users\\clair\\Desktop\\WS_20\\GeoApp"
 #generation of an empty PDF master document
-pdfDoc = arcpy.mp.PDFDocumentCreate(outPath + "")
+pdfDoc = arcpy.mp.PDFDocumentCreate(outPath + "kernelmap")
