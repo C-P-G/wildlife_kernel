c01318f4514d6bacaf710d54e3f40c13d1f97ed9
C-P-G <claire.pfalzner-gibbon@stud.sbg.ac.at>
2022-02-09 14:11:13 +0100

    Kernel function

--- accidentsmain.py
+++ accidentsmain.py
@@ -6,7 +6,11 @@
 
 import arcpy
 # Set environment settings
-arcpy.env.workspace = "C:\\Users\\clair\\Documents\\ArcGIS\\Projects\\wildlifekernel\\wildlifekernel.gdb"
+from arcpy.sa import KernelDensity
+
+# need for saving kernel later
+wildlifeGDB = "C:\\Users\\clair\\Documents\\ArcGIS\\Projects\\wildlifekernel\\wildlifekernel.gdb"
+arcpy.env.workspace = wildlifeGDB
 # otherwise it spits out an error if you run the code more then once with the same output name
 arcpy.env.overwriteOutput = True
 
@@ -14,25 +18,32 @@ arcpy.env.overwriteOutput = True
 # Set local variables
 in_features = "streets"
 # set output path
-output_buffer = "C:\\Users\\clair\\Documents\\ArcGIS\\Projects\\wildlifekernel\\wildlifekernel.gdb\\bufferstreets"
-buffer_distance = "200 meters"
+output_buffer = "C:\\Users\\clair\\Documents\\ArcGIS\\Projects\\wildlifekernel\\wildlifekernel.gdb\\bufferstreets"#buffer_distance = "200 meters"
 # actual buffer function
-arcpy.analysis.Buffer(in_features, output_buffer, buffer_distance)
+#arcpy.analysis.Buffer(in_features, output_buffer, buffer_distance)
 
 
 # Intersect streets with accidents
 
-in_features = ["Roe_deer", "bufferstreets"]
+in_features = ["bufferstreets", "Roe_deer"]
 out_feature_class = "C:\\Users\\clair\\Documents\\ArcGIS\\Projects\\wildlifekernel\\wildlifekernel.gdb\\intersection"
-join_attributes = "all"
-output_type = "point"
-arcpy.analysis.Intersect(in_features, out_feature_class, join_attributes, output_type)
+join_attributes = "ONLY_FID"
+output_type = "polyline"
+arcpy.analysis.Intersect(in_features, out_feature_class, join_attributes)
 
 
 # KERNELFUNCTION
-# which one to use ?
-# https://pro.arcgis.com/de/pro-app/latest/tool-reference/geostatistical-analyst/an-overview-of-the-interpolation-toolset.htm
+in_features = "intersection"
+populationfield = "none"
+out_cell_values = "DENSITIES"
+# leave out cell size and search radius for now, does not work with it
+cell_size = 10
+search_radius = 400
 
+kd = KernelDensity(in_features, populationfield)
+
+# saves the kernel in the database
+kd.save(wildlifeGDB + "\\" + in_features + "_kd")
 
 # PRINT PDF
 # output path (folder). Please change accordingly
