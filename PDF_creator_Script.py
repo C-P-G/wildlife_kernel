b34d3c8c7e7cd629ab6b65fd3e923f791587762a
Felix2109 <94894515+Felix2109@users.noreply.github.com>
2022-03-01 15:49:59 +0100

    Add files via upload

---
+++ PDF_creator_Script.py
@@ -0,0 +1,43 @@
+# imports the Python side package for geographical analysis
+import arcpy
+#link to the Project you want to work on
+aprx = arcpy.mp.ArcGISProject (r"C:\Users\felix\OneDrive\Dokumente\ArcGIS\Projects\Final_Assignment\Final_Assignment.aprx")
+gdb = r"C:\Users\felix\OneDrive\Dokumente\ArcGIS\Projects\Final_Assignment\Final_Assignment.gdb"
+print(gdb)
+m = aprx.listMaps()[0]
+lyrSBG = m.listLayers()[0]
+lyrProject = m.listLayers ()[0]
+
+input = arcpy.GetParameterAsText(0)
+
+#print (lyrSBG)
+#for object in lyrProject:
+#    print(object)
+#lyrKDE = m.listLayers()[2]
+lyt = aprx.listLayouts()[0]
+print(lyt.name)
+mf = lyt.listElements("MAPFRAME_ELEMENT")[0]
+outPath = "C:\\Users\\felix\\OneDrive\\Desktop\\FinalProject_ApplicationDevelopment\\PDF_CREATOR\\"
+pdfDoc = arcpy.mp.PDFDocumentCreate(outPath + "KDE_compiled1.pdf")
+cur = arcpy.da.SearchCursor(lyrSBG.dataSource, ["SHAPE@", "GRIDCODE", "ID"], 'GRIDCODE = 5')
+#cur2 = arcpy.da.SearchCursor(lyrProject, 'OBJECT = Kernel*')
+#for row in cur2:
+#    print(row)
+#del cur2
+# cur1 = arcpy.da.SearchCursor(lyrKDE.dataSource, ["SHAPE@", "GRIDCODE", "ID"], 'GRIDCODE = 4')
+print (lyrSBG.dataSource)
+for row in cur:
+    mf.camera.setExtent(row[0].extent)
+    name = row[2]
+    print(name)
+    aprx.save
+    lyt.exportToPDF(outPath + str(name) +".PDF", 100)
+    pdfDoc.appendPages (outPath + str(name) +".PDF")
+pdfDoc.saveAndClose()
+print ("PDF Mapbook created")
+del pdfDoc
+del aprx
+del m
+del lyt
+del row
+del cur
